4e552bf76d125e48e68526745d81986e694446e6
wemiura <wemiura@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-22 10:29:55 +0000

    Cron Job Fixed
    git-svn-id: https://plugins.svn.wordpress.org/get-your-ebay-feedback/tags/1.3@1227931 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ library/keys.php
@@ -0,0 +1,15 @@
+<?php
+/*  � 2013 eBay Inc., All Rights Reserved */ 
+/* Licensed under CDDL 1.0 -  http://opensource.org/licenses/cddl1.php */
+    //show all errors - useful whilst developing
+    error_reporting(E_ALL);
+
+    // these keys can be obtained by registering at http://developer.ebay.com
+    
+    $production         = true;   // toggle to true if going against production
+    $compatabilityLevel = 903;    // eBay API version
+    $serverUrl = 'https://api.ebay.com/ws/api.dll';
+   
+    
+    
+?>
